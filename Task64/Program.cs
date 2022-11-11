ea9a02d52d7bb743a8513dfd6e45b79932e73e1b
Anton <tepa-86@mail.ru>
2022-11-12 00:20:32 +0300

    Добавление папки Task64: решение задачи

---
+++ Task64/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> [5, 4, 3, 2, 1]
+N = 8 -> [8, 7, 6, 5, 4, 3, 2, 1]*/
+
+// Ввод значения, проверка на правильность ввода
+int EnteringValue(string message)
+{
+	int result = 0;
+	bool isCorrect = false;
+
+	while (!isCorrect)
+	{
+		Console.Write(message);
+		isCorrect = int.TryParse(Console.ReadLine(), out result);
+
+		if (!isCorrect)
+			Console.WriteLine("\nВведите корректное число!\n");
+	}
+	return result;
+}
+
+//Нахождение натуральных чисел в промежутке от N до 1
+string AllNaturalValues(int number)
+{
+	if (number <=0)
+		throw new Exception($"\nПроверьте правильность ввода, числа должны быть натуральным!");
+
+	if (number <= 1)
+		return number.ToString();
+	else
+	{
+		return number + "," + AllNaturalValues(number - 1);
+	}
+}
+
+try
+{
+int value = EnteringValue("\nВведите число N: ");
+Console.WriteLine($"N = {value} -> [{AllNaturalValues(value)}]");
+}
+
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
